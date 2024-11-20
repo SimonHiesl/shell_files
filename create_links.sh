85503eccf7be140b3380f4d51be98370b5053322
Simon Hiesl <simon.hiesl@gmail.com>
2024-11-20 13:02:11 +0100

    mntr update

--- create_links.sh
+++ create_links.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 source_dir="/home/hiesl/shell_files/bash_files"
-target_dir="/usr/local/bin"
+target_dir="/home/hiesl/.local/bin"
 
-# Navigate to the source directory
 cd "$source_dir" || exit
 
-# Loop through each file in the source directory
 for file in *; do
-    # Check if the item is a regular file (not a directory)
     if [ -f "$file" ]; then
-        # Create a symbolic link in the target directory with the same filename
         ln -s "$(realpath "$file")" "$target_dir/$file"
     fi
 done
